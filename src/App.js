7a4580f4f26928bd8221269ec31f13b0c9b1bd43
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2024-03-28 23:32:02 +0300

    Update Css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import "./styles.css";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,18 +11,30 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <button>Button</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="div1">
+          <p className="div1">The existence of the button is being tested.</p>
+          <button>Button</button>
+        </div>
+        <div className="div2">
+          <p className="div2">Routing is being tested.</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </div>
       </header>
-      <h1>{count}</h1>
-      <button onClick={handleInc}>Inc</button>
+      <div className="div3">
+        <p className="div3">
+          The clickability of the button at the user level and the functioning
+          of the func mechanics are being tested.
+        </p>
+        <h1 className="div3">{count}</h1>
+        <button onClick={handleInc}>Inc</button>
+      </div>
     </div>
   );
 }
